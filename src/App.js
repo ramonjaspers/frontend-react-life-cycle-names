a69fcf669f8e80b93bbede2a8eb0fe3105d7e342
ramonjaspers <ramonj360@gmail.com>
2021-09-30 15:19:32 +0200

    solved exercise

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import NameItem from './components/NameItem';
 import './App.css';
 
+
 function App() {
+  const [activeName, setActiveName] = useState('');
+
+
+  useEffect(() => {
+
+    if (!activeName) {
+      console.log('We gaan beginnen');
+    } else {
+      console.log(`${activeName} wil graag een biertje bestellen`);
+
+
+    }
+  }, [activeName]);
+
   return (
     <>
-      <h1>De actieve naam is: ...</h1>
+      <h1>{activeName ? `${activeName} wil graag een biertje bestellen` : "We gaan beginnen"}</h1>
       <ul>
-        <NameItem name="Henk" />
-        <NameItem name="Piet" />
-        <NameItem name="Jan" />
+        <NameItem name="Henk" nameSetter={setActiveName} />
+        <NameItem name="Piet" nameSetter={setActiveName} />
+        <NameItem name="Jan" nameSetter={setActiveName} />
       </ul>
     </>
   );
